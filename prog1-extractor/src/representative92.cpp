b2053ad6dcec7fd474dfb726ea88ae0054b44034
Aaron King <70102182+aaronwk910@users.noreply.github.com>
2022-02-03 18:16:17 -0500

    Add files via upload

---
+++ prog1-extractor/src/representative92.cpp
@@ -0,0 +1,101 @@
+/*
+ * 	representative.cpp
+ *  Created on: Feb 1, 2022
+ *  Author: Aaron King
+ */
+
+#include "representative92.h"
+#include <string>
+
+int phoneNumber::getAreaCodeInt(){
+	return areaCode;
+}
+string phoneNumber::getAreaCodeStr(){
+	return to_string(areaCode);
+}
+string phoneNumber::getPhoneNumber(){ //return full phone number with correct formatting
+	string ret = "(";
+	ret += to_string(areaCode);
+	ret += ")";
+	ret += to_string(remainder1);
+	ret += "-";
+	ret += to_string(remainder2);
+	return ret;
+}
+phoneNumber::phoneNumber(){
+	areaCode = 999;
+	remainder1 = 999;
+	remainder2 = 9999;
+}
+
+string name::getMidInit(){
+	return midInit;
+}
+string name::getTitle(){
+	return title;
+}
+string name::getFirstName(){
+	return firstName;
+}
+string name::getLastName(){
+	return lastName;
+}
+string name::getFullName(){
+	return (title + " " + firstName + " " + midInit + " " + lastName);
+}
+name::name(){
+	midInit = "none";
+	title = "none";
+	firstName = "none";
+	lastName = "none";
+}
+
+
+
+int address::getZipInt(){
+	return zip;
+}
+string address::getZipStr(){
+	return to_string(zip);
+}
+string address::getStreetAdd(){
+	return streetAdd;
+}
+string address::getCity(){
+	return city;
+}
+string address::getAddress(){
+	string ret = "";
+	ret += streetAdd;
+	ret += " ";
+	ret += city;
+	ret += " South Carolina ";
+	ret += to_string(zip);
+	return ret;
+}
+address::address(){
+	zip = 99999;
+	streetAdd = "none";
+	city = "none";
+}
+
+
+string representative::getCounty(){
+	return county;
+}
+string representative::getParty(){
+	return party;
+}
+string representative::getPersonalInfo(){
+	return personalInfo;
+}
+string representative::getDistrict(){
+	return district;
+}
+string representative::getService(){
+	return service;
+}
+representative::representative(){
+	county = party = personalInfo = district = service = "none";
+
+}
